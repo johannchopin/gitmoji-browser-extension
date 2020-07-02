71e11df93756e5a990de4731641227fe5bf5feb4
Lyokolux <lyokolux@protonmail.com>
2020-07-03 00:10:12 +0200

    🚧

--- src/components/Searchbar.svelte
+++ src/components/Searchbar.svelte
@@ -1,22 +1,21 @@
+<style>
+  input {
+    width: 60%;
+    padding: 5px 10px;
+    font-family: Avenir;
+    border: none;
+    border-radius: 4px;
+    box-shadow: 1px 1px 1px rgba(168, 182, 191, 0.6);
+  }
+
+  input:focus {
+    outline: none;
+  }
+</style>
+
 <input
   maxlength="70"
   type="text"
   placeholder="Search your emoji..."
   autofocus
-  on:input
-/>
-
-<style>
-input {
-  width: 100%;
-  padding: 5px 10px;
-  font-family: Avenir;
-  border: none;
-  border-radius: 4px;
-  box-shadow: 1px 1px 1px rgba(168, 182, 191, .6);
-}
-
-input:focus {
-  outline: none;
-}
-</style>
+  on:input />
