168b1d2cefa5d1630618bfb82c8889bf263a799a
Lyokolux <lyokolux@protonmail.com>
2020-07-03 02:35:04 +0200

    ✨(Footer.svelte): make links functional in the footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,25 +1,27 @@
+<style>
+  a {
+    color: black;
+    text-decoration: none;
+  }
+
+  a:not(:first-of-type)::before {
+    content: "-";
+    margin: 0 0.25em;
+  }
+
+  nav {
+    display: flex;
+    justify-content: center;
+    padding: 0.25em 0;
+  }
+</style>
+
 <footer>
   <nav>
     <a href="https://gitmoji.carloscuesta.me/about" target="_blank">About</a>
-    <a href="https://gitmoji.carloscuesta.me/contributors">Contributors</a>
-    <a href="https://github.com/carloscuesta/gitmoji">GitHub</a>
+    <a href="https://gitmoji.carloscuesta.me/contributors" target="_blank">
+      Contributors
+    </a>
+    <a href="https://github.com/carloscuesta/gitmoji" target="_blank">GitHub</a>
   </nav>
 </footer>
-
-<style>
-a {
-  color: black;
-  text-decoration: none;
-}
-
-a:not(:first-of-type)::before {
-  content: '-';
-  margin: 0 .25em;
-}
-
-nav {
-  display: flex;
-  justify-content: center;
-  padding: .25em 0;
-}
-</style>
