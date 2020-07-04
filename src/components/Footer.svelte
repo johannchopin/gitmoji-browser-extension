d9f7b50cee85d01d7218eadaeb22bc78d436c0bb
johannchopin <johannchopin@pm.me>
2020-07-04 16:05:36 +0200

    🚧

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,24 +4,15 @@
     text-decoration: none;
   }
 
-  a:not(:first-of-type)::before {
-    content: "-";
-    margin: 0 0.25em;
-  }
-
   nav {
     display: flex;
-    justify-content: center;
+    justify-content: flex-end;
     padding: 0.25em 0;
   }
 </style>
 
 <footer>
   <nav>
-    <a href="https://gitmoji.carloscuesta.me/about" target="_blank">About</a>
-    <a href="https://gitmoji.carloscuesta.me/contributors" target="_blank">
-      Contributors
-    </a>
-    <a href="https://github.com/carloscuesta/gitmoji" target="_blank">GitHub</a>
+    <a href="https://github.com/johannchopin/gitmoji-browser-extension" target="_blank">GitHub</a>
   </nav>
 </footer>
