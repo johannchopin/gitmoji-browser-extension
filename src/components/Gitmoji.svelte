782ab5361b17b7020b035c2a19d4387a0a9fd312
Lyokolux <lyokolux@protonmail.com>
2020-07-04 15:36:34 +0200

    💄(Gitmoji.svelte): add border radius for code

--- src/components/Gitmoji.svelte
+++ src/components/Gitmoji.svelte
@@ -1,13 +1,13 @@
 <script>
-  export let code
-  export let emoji
-  export let color
-  export let onSave
+  export let code;
+  export let emoji;
+  export let color;
+  export let onSave;
 
   const copyToClipboard = (value) => {
-    navigator.clipboard.writeText(value)
-    onSave()
-  }
+    navigator.clipboard.writeText(value);
+    onSave();
+  };
 </script>
 
 <style>
@@ -75,9 +75,10 @@
   .code:focus span::before {
     position: absolute;
     content: "";
-    margin-top: 1em; /* put it under the text */
     width: 100%;
+    margin-top: 1em; /* put it under the text */
     height: 0.2em;
+    border-radius: 4px;
     background-color: var(--color);
     animation: 0.15s slideRight;
   }
