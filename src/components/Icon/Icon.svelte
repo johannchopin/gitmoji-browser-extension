5532c69d86a1b2b9bf4a27f636be89dec9ddd463
johannchopin <johannchopin@pm.me>
2020-07-20 20:18:49 +0200

    ✨ Add Icon component

---
+++ src/components/Icon/Icon.svelte
@@ -0,0 +1,28 @@
+<script>
+  import eye from './icons/eye.svg'
+
+  export let name;
+  export { className as class };
+  
+  let className = '';
+  let icon;
+
+  $: switch (name) {
+    case 'eye':
+      icon = eye
+      break;
+  
+    default:
+      break;
+  }
+  
+</script>
+
+<style>
+  img {
+    width: 1em;
+  }  
+</style>
+
+
+<img src={icon} alt={`icon: ${name}`} class="icon {name} {className}" />
