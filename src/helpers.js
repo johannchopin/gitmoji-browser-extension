e41153bcd027b86ec9b6e915cf071b638429a978
johannchopin <johannchopin@pm.me>
2020-07-22 19:13:57 +0200

    🥅 Be sure that chrome.tabs is not undefined

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,7 @@
 export const onGetTabInnerHTML = (callback) => {
-  chrome.tabs.executeScript(null, { code: "document.body.innerText" },
-    (results) => { callback(results[0]) }
-  );
+  if (chrome.tabs !== undefined) {
+    chrome.tabs.executeScript(null, { code: "document.body.innerText" },
+      (results) => { callback(results[0]) }
+    );
+  }
 }
