895122bb0abd6cc31105dbbeddcb928ca620b5e1
johannchopin <johannchopin@pm.me>
2020-08-09 14:51:20 +0200

    ✅ Popup

--- test/__tests__/Popup.test.js
+++ test/__tests__/Popup.test.js
@@ -1,10 +1,38 @@
 import { render } from '@testing-library/svelte'
 import Popup from '../../src/Popup.svelte'
 
-describe('index component', () => {
-  test('should render component correctly', () => {
-    const { container } = render(Popup)
+describe('<Popup />', () => {
+  let wrapper
 
-    expect(container).toContainHTML('<div></div>')
+  beforeEach(() => {
+    wrapper = render(Popup)
+  })
+
+  it('should contain a header', () => {
+    const { container } = wrapper
+    expect(container).toContainHTML('<header>')
+  })
+
+  it('should contain gitmoji logo', () => {
+    const { container } = wrapper
+    const imageLink = container.querySelector('header a')
+    const image = imageLink.querySelector('img')
+
+    expect(image).not.toBeNull()
+    expect(imageLink).toHaveAttribute('href', 'https://gitmoji.carloscuesta.me/')
+  })
+
+  it('should contain a searchbar', () => {
+    const { container } = wrapper
+    const searchbar = container.querySelector('header input[type=text]')
+
+    expect(searchbar).not.toBeNull()
+  })
+
+  it('should contain a footer', () => {
+    const { container } = wrapper
+    const footer = container.querySelector('footer')
+
+    expect(footer).not.toBeNull()
   })
 })
