2f4ddee66089801c736a84d7d7d606e78969ca5f
johannchopin <johannchopin@pm.me>
2020-08-09 14:07:02 +0200

    🤡 Mock files

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     '^.+\\.svelte?$': 'jest-transform-svelte'
   },
   moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/__mocks__/fileMock.js',
     '^@/(.*)$': '<rootDir>/src/$1'
   },
   moduleFileExtensions: ['js', 'svelte'],
