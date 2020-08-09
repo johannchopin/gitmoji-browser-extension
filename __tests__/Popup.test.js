5bdd08455e39b96353e735fb2a3a245a713d22f9
johannchopin <johannchopin@pm.me>
2020-08-09 13:28:16 +0200

    ✅ Popup

---
+++ __tests__/Popup.test.js
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/svelte'
+import Popup from '../src/Popup.svelte'
+
+describe('index component', () => {
+  test('should render component correctly', () => {
+    const { container } = render(Popup)
+
+    expect(container).toContainHTML('<div></div>')
+  })
+})
