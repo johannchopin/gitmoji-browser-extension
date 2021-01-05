313872c7758397f0b83ba07238c5a16d6f1683cb
johannchopin <johannchopin@pm.me>
2021-01-05 14:47:48 +0100

    ✨ User settings store

--- src/stores.js
+++ src/stores.js
@@ -1,7 +1,10 @@
 import { writable } from 'svelte/store'
+import { isInExtension } from './helpers/browser'
 
-function createPageStore() {
-  const { subscribe, set } = writable('home')
+const AUTO_CLOSE_AFTER_COPY_KEY = 'auto-close-after-copy'
+
+const createPageStore = () => {
+  const { subscribe, set } = writable('settings')
 
   return {
     subscribe,
@@ -11,4 +14,51 @@ function createPageStore() {
   }
 }
 
+const createUserSettingsStore = () => {
+  const { subscribe, set, update } = writable({ autoCloseAfterCopy: false, theme: 'light' })
+
+  // get settings from storage
+  if (isInExtension()) {
+    /* eslint-disable-next-line no-undef */
+    chrome.storage.sync.get([AUTO_CLOSE_AFTER_COPY_KEY], (result) => {
+      const autoCloseAfterCopyValue = result[AUTO_CLOSE_AFTER_COPY_KEY] || false
+
+      update(settings => {
+        return {
+          ...settings,
+          autoCloseAfterCopy: autoCloseAfterCopyValue
+        }
+      })
+    })
+  }
+
+  return {
+    subscribe,
+    setAutoCloseAfterCopy: (value) => {
+      if (isInExtension()) {
+        /* eslint-disable-next-line no-undef */
+        chrome.storage.sync.set({ [AUTO_CLOSE_AFTER_COPY_KEY]: value })
+      }
+
+      update(settings => {
+        return {
+          ...settings,
+          autoCloseAfterCopy: value
+        }
+      })
+    },
+    setTheme: (theme) => {
+      update(settings => {
+        return {
+          ...settings,
+          theme
+        }
+      })
+    },
+    set,
+    update
+  }
+}
+
 export const page = createPageStore()
+export const settings = createUserSettingsStore()
