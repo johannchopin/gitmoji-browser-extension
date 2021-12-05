d0d02f5ac84438dd9633c1d0b32ad66fee9ef468
johannchopin <johannchopin@pm.me>
2021-12-05 11:29:53 +0100

    ✨ Add focus command UI

--- src/components/Searchbar.svelte
+++ src/components/Searchbar.svelte
@@ -1,13 +1,41 @@
+<script>
+  import { isMacOs } from '../helpers/browser'
+</script>
+
 <style>
-  input {
+  .input {
+    display: flex;
     width: 60%;
     padding: 5px 10px;
     border: none;
     border-radius: 4px;
+    background-color: white;
     box-shadow: 1px 1px 1px rgba(168, 182, 191, 0.6);
   }
 
-  :global(body.dark) input {
+  .input:focus-within {
+    outline: -webkit-focus-ring-color auto 1px;
+  }
+
+  input {
+    border: none;
+    width: 100%;
+  }
+
+  kbd {
+    align-items: center;
+    border-radius: 3px;
+    border: solid 1px black;
+    color: black;
+    display: flex;
+    padding: 0.25rem;
+    white-space: nowrap;
+    font-family: Avenir;
+    font-weight: bold;
+    font-size: 1.2em;
+  }
+
+  :global(body.dark) .input {
     color: white;
     background-color: black;
   }
@@ -18,9 +46,14 @@
 </style>
 
 <!-- svelte-ignore a11y-autofocus -->
-<input
-  maxlength="70"
-  type="text"
-  placeholder="Search your gitmoji..."
-  autofocus
-  on:input />
+<div class="input">
+  <input
+    maxlength="70"
+    type="text"
+    placeholder="Search your gitmoji..."
+    autofocus
+    on:input
+  />
+
+  <kbd>{isMacOs() ? '⌘' : 'Ctrl'} K</kbd>
+</div>
